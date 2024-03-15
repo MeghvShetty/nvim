69d036c08d56a3a8e561e8bbb0bbdecc7a2c6876
Megh Shetty <meghvshetty@outlook.com>
2024-03-15 17:53:00 +0000

    update the lsp config & removed goinit

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return{
             "williamboman/mason-lspconfig.nvim",
             config = function()
                 require("mason-lspconfig").setup({
-                    ensure_installed = {"lua_ls","gopls","golangci_lint_ls"}
+                    ensure_installed = {"lua_ls","gopls"}
                 })
             end
         }
@@ -20,7 +20,6 @@ return{
         config = function()
         local lspconfig = require('lspconfig')
         lspconfig.lua_ls.setup({})
-        lspconfig.golangci_lint_ls.setup({})
         lspconfig.gopls.setup({})
         vim.keymap.set('n','K', vim.lsp.buf.hover,{})
         vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
