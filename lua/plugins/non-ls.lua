4ce300b656919c3cb6f8a74f26b7b5e798185b6d
Megh Shetty <meghvshetty@outlook.com>
2024-03-15 20:34:54 +0000

    comment javascript-formatter

--- lua/plugins/non-ls.lua
+++ lua/plugins/non-ls.lua
@@ -5,11 +5,17 @@ return {
 
 		null_ls.setup({
 			sources = {
+				--Lua
 				null_ls.builtins.formatting.stylua,
+				--Golang
 				null_ls.builtins.formatting.gofmt,
-
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.diagnostics.golangci_lint,
+				--Javascript
+				--null_ls.builtins.formatting.prettier,
+				--null_ls.builtins.diagnostics.eslint_d,
 			},
 		})
-	vim.keymap.set('n','<leader>cf', vim.lsp.buf.format,{})
-	end
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+	end,
 }
