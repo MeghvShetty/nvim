5bff0d0d8ee4b3575e34711d4e4679f16b53633a
Megh Shetty <meghvshetty@outlook.com>
2024-03-15 22:38:40 +0000

    added key mapper

--- init.lua
+++ init.lua
@@ -3,6 +3,16 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.opt.number = true
 vim.o.breakindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.o.termguicolors = true
+
+
+--splite screen 
+vim.keymap.set("n", "<leader>s", ":vsplit<CR><C-w>l", { noremap = true })
+vim.keymap.set("n", "<leader>h", ":wincmd h<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>l", ":wincmd l<CR>", { noremap = true })
+
 -- Lazy Vim package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
