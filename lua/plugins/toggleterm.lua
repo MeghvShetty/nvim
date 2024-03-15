e2245237af22ef1a7bdce62abe1e2c7c5d18c33c
Megh Shetty <meghvshetty@outlook.com>
2024-03-15 09:31:56 +0000

    added a new plug in

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,15 @@
+return {
+  -- amongst your other plugins
+  	{
+	'akinsho/toggleterm.nvim',
+	version = "*",
+	config = true
+	};
+	--config = function()
+	--local config = require("toggleterm").setup{
+	--direction = "horizontal",
+	--size = 90,
+	--open_mapping = [[<M-j>]]
+	--}
+	--end
+}
