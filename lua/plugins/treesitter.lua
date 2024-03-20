601025e9a385a6cc08eece442d6adf751af20824
Megh Shetty <meghvshetty@outlook.com>
2024-03-20 13:13:25 +0000

    formatter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,16 @@
-return { 
-    {
-    "nvim-treesitter/nvim-treesitter",
-    build =":TSUpdate",
-    -- Treesitter config
-    config = function()
-local config = require("nvim-treesitter.configs")
-config.setup ({
-	ensure_installed ={"lua","go","javascript"},
-	highlight = {enable = true},
-	indent = {enable = true}
-    })
-    end
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		-- Treesitter config
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
-}
+
