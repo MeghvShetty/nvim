a9a9d96c9fb5158ffb8a05ccc9ce504b7b855192
Megh Shetty <meghvshetty@outlook.com>
2024-06-15 21:35:56 +0100

    code clean up

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,49 +1,95 @@
-return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
+-- return {
+-- 	{
+-- 		"hrsh7th/cmp-nvim-lsp",
+-- 	},
+-- 	{
+-- 		"L3MON4D3/LuaSnip",
+-- 		dependencies = {
+-- 			"saadparwaiz1/cmp_luasnip",
+-- 			"rafamadriz/friendly-snippets",
+-- 		},
+-- 	},
+-- 	{
+-- 		"hrsh7th/nvim-cmp",
+-- 		config = function()
+-- 			-- Set up nvim-cmp.
+-- 			local cmp = require("cmp")
+-- 			-- require("luasnip.loaders.from_vscode").lazy_load()
+-- 			require("luasnip.loaders.from_vscode").load {
+-- 				exclude = { "go","lua" },
+-- 			}
+
+-- 			cmp.setup({
+-- 				snippet = {
+-- 					-- REQUIRED - you must specify a snippet engine
+-- 					expand = function(args)
+-- 						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+-- 					end,
+-- 				},
+-- 				window = {
+-- 					completion = cmp.config.window.bordered(),
+-- 					documentation = cmp.config.window.bordered(),
+-- 				},
+-- 				mapping = cmp.mapping.preset.insert({
+-- 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+-- 					["<C-f>"] = cmp.mapping.scroll_docs(4),
+-- 					["<C-Space>"] = cmp.mapping.complete(),
+-- 					["<C-e>"] = cmp.mapping.abort(),
+-- 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+-- 				}),
+-- 				sources = cmp.config.sources({
+-- 					{ name = "luasnip" }, -- For luasnip users.
+-- 				}, {
+-- 					{ name = "buffer" },
+-- 				}),
+-- 			})
+-- 		end,
+-- 	},
+-- }
+
+return{
+	{ 'hrsh7th/vim-vsnip' },
 	{
 		"hrsh7th/nvim-cmp",
-		config = function()
-			-- Set up nvim-cmp.
-			local cmp = require("cmp")
-			-- require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_vscode").load {
-				exclude = { "go","lua" },
-			}
-
-			cmp.setup({
-				snippet = {
-					-- REQUIRED - you must specify a snippet engine
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+		},
+		config =function ()
+		local cmp = require'cmp'
+	cmp.setup {
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end,
+    },
+    mapping = {
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' }, -- For vsnip users.
+    }, {
+        { name = 'buffer' },
+    })
+}
+end,
 	},
 }
 
