cd7f0286870e352687cae85bbcc26e16eddae102
Megh Shetty <meghvshetty@outlook.com>
2025-12-07 22:36:05 +0000

    dissable venv-lsp

--- lua/plugins/plugins-oneliners.lua
+++ lua/plugins/plugins-oneliners.lua
@@ -1,22 +1,21 @@
-return{
-    { -- This helps with ssh tunneling and copying to clipboard
-	'ojroques/vim-oscyank',
-    },
-    { --git plugin
-	'tpope/vim-fugitive',
-    },
-    { --show CSS Colors
-	'brenoprata10/nvim-highlight-colors',
-	config = function()
-	    require('nvim-highlight-colors').setup({})
-	end
-    },
-    {
-  "jglasovic/venv-lsp.nvim",
-  config = function()
-    require("venv-lsp").setup()
-  end,
-    },
-    
-
+return {
+	{ -- This helps with ssh tunneling and copying to clipboard
+		"ojroques/vim-oscyank",
+	},
+	{ --git plugin
+		"tpope/vim-fugitive",
+	},
+	{ --show CSS Colors
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({})
+		end,
+	},
+	{
+		"jglasovic/venv-lsp.nvim",
+		enabled = false,
+		config = function()
+			require("venv-lsp").setup()
+		end,
+	},
 }
