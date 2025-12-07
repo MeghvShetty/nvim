aa0412b45319a06ef38d7028bdef518780f3aa9f
Megh Shetty <meghvshetty@outlook.com>
2025-12-07 22:35:33 +0000

    clipbard added

--- init.lua
+++ init.lua
@@ -3,3 +3,5 @@ print("Welcome to Neovim Master Megh!!")
 require("config.options")
 require("config.keybinds")
 require("config.lazy")
+
+vim.opt.clipboard = "unnamedplus" -- yy/p copies to system clipboard automatically
