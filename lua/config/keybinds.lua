fc3a99f5d3b358134b155fe5a3cb8cf7899be770
Megh Shetty <meghvshetty@outlook.com>
2025-12-07 17:37:24 +0000

    Updated python parth

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = " "
-vim.keymap.set("n","<leader>cd", vim.cmd.Ex) -- n = normal mode option2 = keymap option3 = what command should it execute
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex) -- n = normal mode option2 = keymap option3 = what command should it execute
+--vim.g.python3_host_prog = "/Users/meghshetty/Documents/Python-reborn/.venv/bin/pip3.14"
