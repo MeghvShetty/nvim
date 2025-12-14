a6f02f526fc5fe9b8e3fee8ea6a1598a5b4c642c
Megh Shetty <meghvshetty@outlook.com>
2025-12-14 15:23:11 +0000

    added markdown support

--- lua/plugins/plugins-oneliners.lua
+++ lua/plugins/plugins-oneliners.lua
@@ -1,21 +1,27 @@
 return {
-	{ -- This helps with ssh tunneling and copying to clipboard
-		"ojroques/vim-oscyank",
-	},
-	{ --git plugin
-		"tpope/vim-fugitive",
-	},
-	{ --show CSS Colors
-		"brenoprata10/nvim-highlight-colors",
-		config = function()
-			require("nvim-highlight-colors").setup({})
-		end,
-	},
-	{
-		"jglasovic/venv-lsp.nvim",
-		enabled = false,
-		config = function()
-			require("venv-lsp").setup()
-		end,
-	},
+    { -- This helps with ssh tunneling and copying to clipboard
+        "ojroques/vim-oscyank",
+    },
+    { --git plugin
+        "tpope/vim-fugitive",
+    },
+    { --show CSS Colors
+        "brenoprata10/nvim-highlight-colors",
+        config = function()
+            require("nvim-highlight-colors").setup({})
+        end,
+    },
+    {
+        "jglasovic/venv-lsp.nvim",
+        enabled = false,
+        config = function()
+            require("venv-lsp").setup()
+        end,
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app && npm install",
+        ft = "markdown",
+        cmd = "MarkdownPreview",
+    },
 }
